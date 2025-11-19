cd9dd22008f5c10c2eda35991c3ae9030c900bc9
salva <200050789@studenti.uniupo.it>
2025-11-19 12:15:05 +0100

    update JDBC

--- src/main/java/DB/PostGreSQL.java
+++ src/main/java/DB/PostGreSQL.java
@@ -5,18 +5,25 @@ import java.sql.*;
 public class PostGreSQL {
     public static void main(String[] args){
 
-        // Register the PostgreSQL driver
         try{
-            String jdbcUrl = "jdbc:postgresql://localhost:5432/database_name";
-            String username = "username";
-            String password = "password";
+            // Register the PostgreSQL driver
+            String jdbcUrl = "jdbc:postgresql://localhost:5432/autostrada";
+            String username = "postgres";
+            String password = "admin";
             Connection con;
             Statement st;
             Class.forName("org.postgresql.Driver");
+
             // Connect to the database
             con = DriverManager.getConnection(jdbcUrl, username, password);
+
             // Perform desired database operations
             st= con.createStatement();
+            String query = "select * from Elenco";
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next()){
+                System.out.println(rs.getString(2)); //tutta la colonna 2 (secondo campo)
+            }
             // Close the connection
             con.close();
         }
