b52814a93506391153fc6a86668d83ebd0f4f621
emanarratone <emanarratone@gmail.com>
2025-11-20 13:58:13 +0100

    insertBiglietto

--- src/main/java/DB/daoBiglietto.java
+++ src/main/java/DB/daoBiglietto.java
@@ -1,14 +1,12 @@
 package DB;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import model.Autostrada.Biglietto;
+
+import java.sql.*;
 
 public class daoBiglietto {
 
     public static long countAutoOggi() throws SQLException {
-
         String SQL =
                 "SELECT COUNT(*) AS auto_oggi " +
                         "FROM Biglietto " +
@@ -22,6 +20,24 @@ public class daoBiglietto {
                 return rs.getLong("auto_oggi");
             }
             return 0L;
+        }
+    }
+
+    public void insertBiglietto(Biglietto biglietto) throws SQLException {
+
+        String sql = "INSERT INTO BIGLIETTO (matricola, id_totem, targa_auto, timestamp_in, id_casello_in) " +
+                "VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection con = DbConnection.getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, biglietto.getMatricola());
+            ps.setString(2, biglietto.getID_Totem());
+            ps.setString(3, biglietto.getAuto().getTarga());
+            ps.setTimestamp(4, Timestamp.valueOf(biglietto.getTimestamp_in()));
+            ps.setString(5, biglietto.getCasello_in().getSigla());
+
+            ps.executeUpdate();
         }
     }
 }
